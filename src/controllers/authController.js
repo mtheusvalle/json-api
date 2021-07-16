0035e29c113f5523b4208b60f282084f6ace3dc0
mtheusvalle <55810334+mtheusvalle@users.noreply.github.com>
2021-07-16 09:45:38 -0300

    Create authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
 
 const User = require('../models/user');
 
 const router = express.Router();
+
+const generateToken = (params = {}) => {
+    return jwt.sign(params, authConfig.secret, { 
+        expiresIn: 86400 
+    });
+}
 
 router.post('/register', async (req, res) => {
     try {
@@ -15,10 +25,34 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
 
-        return res.send({ user });
+        res.send({ 
+            user,
+            token: generateToken({ id: user.id }),
+         });
     } catch (err) {
         return res.status(400).send({ error: 'Registration Failed!' });
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+        return res.status(400).json({ error: 'User not found' });
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).json({ error: 'Incorrect password' });
+
+    user.password = undefined;
+
+    const token = 
+
+    res.send({ 
+        user, 
+        token: generateToken({ id: user.id }), 
+    });
 });
 
 module.exports = app => app.use('/auth', router);
